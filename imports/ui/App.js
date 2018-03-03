a64d6ac6833ee37cbd1e4171eb7da28b4031c175
Ataeraxia <you@example.com>
2018-03-03 16:06:08 -0500

    Add and hook up new Notes form.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import { withTracker } from "meteor/react-meteor-data";
 
 import { Notes } from "../api/notes.js";
@@ -6,6 +7,18 @@ import { Notes } from "../api/notes.js";
 import Note from './components/Note.js';
 
 class App extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Notes.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
     renderNotes() {
         return this.props.notes.map((note) => (
             <Note key={note._id} note={note} />
@@ -20,6 +33,13 @@ class App extends Component {
                         Captain's Log
                     </h1>
                 </header>
+
+                <form className="new-note"
+                      onSubmit={this.handleSubmit.bind(this)}>
+                    <input type="text"
+                           ref="textInput"
+                           placeholder="Write down your thoughts" />
+                </form>
 
                 <ul>
                     {this.renderNotes()}
