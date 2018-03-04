69fb26c7ee58d267e6ff16749f97fd42bccfdabb
Ataeraxia <you@example.com>
2018-03-04 10:40:18 -0500

    Add delete ability. Also add very useless opacity toggle. Why? Don't ask.

--- imports/ui/components/Note.js
+++ imports/ui/components/Note.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 
+import { Notes } from "../../api/notes.js";
+
 export default class Note extends Component {
+    toggleHidden() {
+        Notes.update(this.props.note._id, {
+            $set: {
+                hidden: !this.props.note.hidden
+            },
+        });
+    }
+
+    deleteThisNote() {
+        Notes.remove(this.props.note._id);
+    }
+
     render() {
+        const noteClassName = this.props.note.hidden ? 'hidden' : '';
+
         return (
-            <li>
+            <li className={noteClassName}>
                 <span className="note-text">{this.props.note.text}</span>
-                <span className="date">{this.props.note.createdAt.toLocaleString("en-CA", {
-                    hour12: true,
-                    month: "short",
-                    day: "numeric",
-                    year: "numeric",
-                    hour: "numeric",
-                    minute: "numeric",
-                })}</span>
+                <div className="note-extras">
+                    <span className="date">{this.props.note.createdAt.toLocaleString("en-CA", {
+                        hour12: true,
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                    })}</span>
+                    <button className="hide" onClick={this.toggleHidden.bind(this)}>
+                        hide
+                    </button>
+
+                    <button className="delete" onClick={this.deleteThisNote.bind(this)}>
+                        &times;
+                    </button>
+                </div>
             </li>
         );
     }
