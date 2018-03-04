cbf0f84c82f4261b8a5f03365ccd76375dc019c4
Ataeraxia <you@example.com>
2018-03-04 10:04:26 -0500

    Format dates and style items

--- imports/ui/components/Note.js
+++ imports/ui/components/Note.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 export default class Note extends Component {
     render() {
         return (
-            <li>{this.props.note.text}</li>
+            <li>
+                <span className="note-text">{this.props.note.text}</span>
+                <span className="date">{this.props.note.createdAt.toLocaleString("en-CA", {
+                    hour12: true,
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                })}</span>
+            </li>
         );
     }
 }
